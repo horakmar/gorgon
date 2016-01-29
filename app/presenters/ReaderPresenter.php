2a15369880e381a8a3e254f1a5ff850c894a7587
Martin Horak <horak.martin@gmail.com>
2016-01-29 15:11:11 +0100

    Pass.

--- app/presenters/ReaderPresenter.php
+++ app/presenters/ReaderPresenter.php
@@ -3,6 +3,7 @@
 namespace App\Presenters;
 
 use Nette;
+use Nette\Utils\Json;
 
 class ReaderPresenter extends BaseRacePresenter
 {
@@ -19,8 +20,19 @@ class ReaderPresenter extends BaseRacePresenter
 	public function renderReader(){
 		$this->template->method = $this->httpRequest->getMethod();
 		if($this->template->method == 'POST'){
-			$this->template->data = $this->httpRequest->getPost();
+			$jsondata = $this->httpRequest->getRawBody();
+			$data = Json::decode($jsondata);
+			$this->template->si_number = $data->si_number;
+			$this->template->tm_clear = $data->tm_clear;
+			$this->template->tm_check = $data->tm_check;
+			$this->template->tm_start = $data->tm_start;
+			$this->template->tm_finish = $data->tm_finish;
+			$this->template->punches = [];
+			foreach($data->punches as $punch){
+				$cn = $punch->cn;
+				$time = $punch->time;
+				$this->template->punches[] = ['cn' => $cn, 'time' => $time];
+			}	
 		}
 	}
-
 }
